6ce00cedc926be5da5bdc44d4567ce1704fb73cc
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-01-06 22:58:56 +0530

    Did some data visualization and graphs

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from enum import unique
+from ssl import Options
 from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from pandas.core.dtypes.common import classes
 from sqlalchemy.sql.functions import count
 import datetime
-
+import psycopg2
+import pandas as pd
+from bokeh.plotting import figure, show
+from bokeh.embed import components
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://new:123@localhost/mood_logger'
 db=SQLAlchemy(app)
 
+# Creating a dataframe from the database
+param_dic = {
+    "host"      : "localhost",
+    "database"  : "mood_logger",
+    "user"      : "new",
+    "password"  : "123"
+}
 class Record(db.Model):
     __tablename__="logs"
     id=db.Column(db.Integer, primary_key=True)
@@ -21,11 +33,16 @@ class Record(db.Model):
         self.mood=mood_
         self.timestamp=timestamp_
 
+
+conn = psycopg2.connect(**param_dic)
+df=pd.read_sql("SELECT * FROM logs",conn, parse_dates=['timestamp'])
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/submit_page",methods=["POST"])
+@app.route("/submit",methods=["POST"])
 def submit():
     if request.method=='POST':
         name=request.form['username']
@@ -34,10 +51,15 @@ def submit():
         record=Record(name,mood,timestamp) 
         db.session.add(record)
         db.session.commit()
-        print(record)
-        return render_template("submit_page.html")
-
-
+        # Average ratings per day
+        df["Day"] = df[(df['name']==name)]['timestamp'].dt.date
+        day_avg=df.groupby(['Day']).mean()
+        plot = figure(plot_width=400, plot_height=400,title=None, toolbar_location="below")
+        plot.line(day_avg.index,day_avg['mood'])
+        script, div = components(plot)  
+        kwargs = {'script': script, 'div': div}
+        kwargs['title'] = 'bokeh-with-flask'
+        return render_template("submit_page.html", **kwargs)
 
 if __name__=="__main__":
     app.debug=True
